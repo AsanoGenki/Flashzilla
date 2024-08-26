ab42a0d8e1dbfb5985b5120c362486a7a6613165
AsanoGenki <asanogenki0218@icloud.com>
2024-08-26 16:23:45 +0900

    Initial commit.

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Genki on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
